37749de51d3de8982b817a93468b57ae832dc8e9
Leonh <leonhardsommer96@gmal.com>
2020-09-13 22:23:22 +0200

    changed output device index

--- jukebox.py
+++ jukebox.py
@@ -104,6 +104,7 @@ def play_final():
 	audio_stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 						  channels=wf.getnchannels(),
 						  rate=wf.getframerate(),
+						  output_device_index=1,
 						  output=True)
 
 	keys = ['q', 'w', 'a']
